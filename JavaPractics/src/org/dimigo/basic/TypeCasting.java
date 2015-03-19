cf430a8621ffc21cf97b7f70db493e57d1cf4d7c
소희 <@sohee-pc>
2015-03-19 11:52:57 +0900

    ..

---
+++ JavaPractics/src/org/dimigo/basic/TypeCasting.java
@@ -0,0 +1,23 @@
+package org.dimigo.basic;
+
+/* ***********************************
+ *   << 두번째 수행평가 >>
+ *  내용 : 디미베네
+ *  작성일 : 2015.03.16
+ *************************************/
+
+public class TypeCasting {
+
+	public static void main(String[] args) {
+		long m = 1700000;
+		int e = 3;
+		int s = 1500;
+		String.format("%,d", 10000);
+		System.out.println("<< 디미베네 연간 인건비 >>");
+		System.out.println("월 평균 급여 : " + m + "원");
+		System.out.println("점포 내 직원 수 : " + e + "명");
+		System.out.println("점포 수 : " + s + "개");
+		System.out.println("연간 : ");
+	}
+
+}
