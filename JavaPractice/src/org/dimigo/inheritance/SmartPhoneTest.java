3709f8792c366eda665875234a59648ff7c6c6e7
소희 <@sohee-pc>
2015-08-31 19:46:12 +0900

    스마트폰

---
+++ JavaPractice/src/org/dimigo/inheritance/SmartPhoneTest.java
@@ -0,0 +1,24 @@
+package org.dimigo.inheritance;
+
+public class SmartPhoneTest {
+
+	public static void main(String[] args) {
+		
+		SmartPhone[] smartphone = {
+				new IPhone("iPhone 6", "애플", 700000),
+				new Galaxy("갤럭시 S6", "삼성", 650000)
+		};
+		
+		for(SmartPhone phone : smartphone) {
+			System.out.println(phone.toString());
+			phone.turnOn();
+			phone.pay();
+			phone.useSpecialFunction();
+			phone.turnOff();
+			
+			System.out.println("");
+		}
+
+	}
+
+}
